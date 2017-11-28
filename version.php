1b3acd0571b345d7edeccd2cf2d5fbb170331d2f
Kathrin Osswald <kathrin.osswald@uni-ulm.de>
2017-11-28 09:25:32 +0100

    Release v3.3-r1

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_boost_campus';
-$plugin->version = 2017111700;
-$plugin->release = 'v3.2-r6';
+$plugin->version = 2017112800;
+$plugin->release = 'v3.3-r1';
 $plugin->requires = 2017051501;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array('theme_boost' => 2017051500);
